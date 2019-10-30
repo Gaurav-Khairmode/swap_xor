c23c02a767c1d632e7e1fb1e49fa606d2bd3b13e
Gaurav-Khairmode <57167440+Gaurav-Khairmode@users.noreply.github.com>
2019-10-30 13:40:42 +0530

    Create swap3.c

---
+++ swap3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int a,b;
+    printf("Enter the data of a and b\n");
+    scanf("%d  %d",&a,&b);
+    printf("Before conversion\na=%d\nb=%d\n",a,b);
+    a^=b;
+    b^=a;
+    a^=b;
+    printf("After conversion\na=%d\nb=%d\n",a,b);
+    return 0;
+
+}
